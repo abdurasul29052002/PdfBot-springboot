5fc038f95851f9f3afd02894487cbbda7ccac4d0
Abdurasul <abdurasulabduraimov22@gmail.com>
2023-01-06 19:04:11 +0500

    bugs fixed

--- src/main/java/com/example/pdfbotspringboot/config/BotConfig.java
+++ src/main/java/com/example/pdfbotspringboot/config/BotConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @Data
-@PropertySource("application.yml")
+@PropertySource("classpath:application.yml")
 public class BotConfig {
     @Value("${token}")
     private String token;
