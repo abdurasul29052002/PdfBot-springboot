a99cbb4755f8d74944eea1b5f548eb1ceded0541
Abdurasul <abdurasulabduraimov22@gmail.com>
2023-01-07 10:26:46 +0500

    ZipService added

---
+++ src/main/java/com/example/pdfbotspringboot/service/ZipService.java
@@ -0,0 +1,11 @@
+package com.example.pdfbotspringboot.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.zip.ZipOutputStream;
+
+@Service
+public class ZipService {
+    public void zip() {
+    }
+}
